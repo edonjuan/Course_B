95b291cd519e902b3ff1986b1f69a2ad9337fdf6
Eduardo Donjuan <mario.donjuan@uteq.edu.mx>
2024-10-16 12:02:38 -0600

    mesh

---
+++ cursoMESH/cursoMESH.ino
@@ -0,0 +1,48 @@
+#include "painlessMesh.h"
+
+#define   MESH_PREFIX     "UTEQ-INTEL"
+#define   MESH_PASSWORD   "UTEQ-INTEL"
+#define   MESH_PORT       5555
+#define   MASTER          2440612283
+#define   YOUR_NAME       "Eduardo Donjuan"     // <-- Change to your name!
+
+Scheduler userScheduler;
+painlessMesh  mesh;
+
+void sendMessage();
+Task taskSendMessage( TASK_SECOND * 10 , TASK_FOREVER, &sendMessage );
+
+void sendMessage() {
+  String msg = "Hello i'm ";
+  msg += YOUR_NAME;
+  msg += " from node: ";
+  msg += mesh.getNodeId();
+  mesh.sendSingle(MASTER, msg);
+  taskSendMessage.setInterval( random( TASK_SECOND * 10, TASK_SECOND * 15 ));
+}
+
+// Needed for painless library
+void receivedCallback( uint32_t from, String &msg ) {
+  Serial.printf("Received from %u msg=%s\n", from, msg.c_str());
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));  // change led state
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  //mesh.setDebugMsgTypes( ERROR | STARTUP );
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
+  mesh.onReceive(&receivedCallback);
+  
+  Serial.print("MI ID ES: ");
+  Serial.println(mesh.getNodeId());
+  
+  userScheduler.addTask( taskSendMessage );
+  taskSendMessage.enable();
+}
+
+void loop() {
+  mesh.update();
+}
